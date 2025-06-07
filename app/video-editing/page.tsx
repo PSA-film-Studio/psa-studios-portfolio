dc503d2219f09eff4cb3ec66b1a7caecd188e992
PSA-film-Studio <psa.film.studios@gmail.com>
2025-06-07 23:23:46 +0530

    Update page.tsx

--- app/video-editing/page.tsx
+++ app/video-editing/page.tsx
@@ -163,7 +163,7 @@ export default function VideoEditingPage() {
           </motion.div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {projects.map((project, index) => (
+            {editingStyles.map((style, index) => (
               <motion.div
                 key={project.id}
                 initial={{ opacity: 0, y: 30 }}
